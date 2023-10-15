4808a41c5e36c487b2a304b7bef9c19ce92a9d92
shashi <shashi863788@gmail.com>
2023-10-15 10:33:20 +0530

    fist commit

---
+++ basics.cpp
@@ -0,0 +1,116 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+// int x=5;
+// //int y=2;
+// //print 
+// //cout<<x/y;
+// ++x;
+// cout<
+// int a=10;
+//  int b=3;
+//  int q = a%b;
+//  int r =a-b*q;
+//  cout<<q;
+//  //cout<<r;
+// int x=10;
+// int y=3;
+// float a = x/y;
+// cout<<a;
+// int a=4;
+// int b=5;
+// cout<<(a<b);
+// int _12=5;
+// int p=100 ,r=2,t=2;
+// int i=(p*r*t)/100;
+// cout<<i;
+// int x;
+// cin>>x;
+// cout<<x;
+// int x;
+// int y;
+// cin>>x;
+// cin>>y;
+// int a=x+y;
+// a=x-y;
+// cout<<a;
+// int r;
+// cin>>r;
+// int pi=22/7;
+// int c=pi*r*r;
+// // cout<<c;
+// int a=5;
+// int b=7;
+// cout<<(a==b)<<endl;
+// cout<<(a<b)<<endl;
+// cout<<(a>b)<<endl;
+// cout<<(a!=b)<<endl;
+// cout<<(a<=b)<<endl;
+// cout<<(a>=b)<<endl;
+// int a=55;
+// int b=66;
+// cout<<a+b;
+// int boy=16;
+// if(boy>18){
+//     cout<<"vote de payega";
+// }
+// else{
+//     cout<<"vote nhi de payega";
+// }
+// int b;
+// cin>>b;
+// if(b%2==0){
+//     cout<<"true";
+//     }
+// else{
+// cout<<"false";
+// }
+// int a;
+// cin>>a;
+// if(a>90){
+//     cout<<"very good";
+    
+// }
+// else if(a>80){
+//     cout<<"good";
+// }
+// else if(a>60){
+//     cout<<"normal";
+// }
+// else{
+//     cout<<"fail";
+// // }
+// int a;
+
+// cin>>a;
+// cout<<"enter my no: "; 
+// if(a>0){
+//     cout<<"positive";
+// }
+// else{
+//     cout<<"nagetive";
+// }
+// int a;
+// int b;
+// cin>>a;
+// cin>>b;
+// if (a<b){
+//     cout<<"profit"<<endl;
+//     cout<<(b-a);
+// }
+// else{
+//     cout<<"lose"<<endl;
+//     cout<<(a-b);
+// }
+
+
+
+
+
+
+
+
+    return 0;
+}
