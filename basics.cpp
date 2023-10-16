5a8abe3c78b699ae68fe3fa23015eb3b9f2eb75e
shashi <shashi863788@gmail.com>
2023-10-16 20:44:51 +0530

    16-10-23 today programe

--- basics.cpp
+++ basics.cpp
@@ -1,164 +1,270 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
- 
+
 int main()
 {
-// int x=5;
-// //int y=2;
-// //print 
-// //cout<<x/y;
-// ++x;
-// cout<
-// int a=10;
-//  int b=3;
-//  int q = a%b;
-//  int r =a-b*q;
-//  cout<<q;
-//  //cout<<r;
-// int x=10;
-// int y=3;
-// float a = x/y;
-// cout<<a;
-// int a=4;
-// int b=5;
-// cout<<(a<b);
-// int _12=5;
-// int p=100 ,r=2,t=2;
-// int i=(p*r*t)/100;
-// cout<<i;
-// int x;
+    // int x=5;
+    // //int y=2;
+    // //print
+    // //cout<<x/y;
+    // ++x;
+    // cout<
+    // int a=10;
+    //  int b=3;
+    //  int q = a%b;
+    //  int r =a-b*q;
+    //  cout<<q;
+    //  //cout<<r;
+    // int x=10;
+    // int y=3;
+    // float a = x/y;
+    // cout<<a;
+    // int a=4;
+    // int b=5;
+    // cout<<(a<b);
+    // int _12=5;
+    // int p=100 ,r=2,t=2;
+    // int i=(p*r*t)/100;
+    // cout<<i;
+    // int x;
+    // cin>>x;
+    // cout<<x;
+    // int x;
+    // int y;
+    // cin>>x;
+    // cin>>y;
+    // int a=x+y;
+    // a=x-y;
+    // cout<<a;
+    // int r;
+    // cin>>r;
+    // int pi=22/7;
+    // int c=pi*r*r;
+    // // cout<<c;
+    // int a=5;
+    // int b=7;
+    // cout<<(a==b)<<endl;
+    // cout<<(a<b)<<endl;
+    // cout<<(a>b)<<endl;
+    // cout<<(a!=b)<<endl;
+    // cout<<(a<=b)<<endl;
+    // cout<<(a>=b)<<endl;
+    // int a=55;
+    // int b=66;
+    // cout<<a+b;
+    // int boy=16;
+    // if(boy>18){
+    //     cout<<"vote de payega";
+    // }
+    // else{
+    //     cout<<"vote nhi de payega";
+    // }
+    // int b;
+    // cin>>b;
+    // if(b%2==0){
+    //     cout<<"true";
+    //     }
+    // else{
+    // cout<<"false";
+    // }
+    // int a;
+    // cin>>a;
+    // if(a>90){
+    //     cout<<"very good";
+
+    // }
+    // else if(a>80){
+    //     cout<<"good";
+    // }
+    // else if(a>60){
+    //     cout<<"normal";
+    // }
+    // else{
+    //     cout<<"fail";
+    // // }
+    // int a;
+
+    // cin>>a;
+    // cout<<"enter my no: ";
+    // if(a>0){
+    //     cout<<"positive";
+    // }
+    // else{
+    //     cout<<"nagetive";
+    // }
+    // int a;
+    // int b;
+    // cin>>a;
+    // cin>>b;
+    // if (a<b){
+    //     cout<<"profit"<<endl;
+    //     cout<<(b-a);
+    // }
+    // else{
+    //     cout<<"lose"<<endl;
+    //     cout<<(a-b);
+    // }
+    // int a;
+    // cout<<"enter a no :";
+    // cin>>a;
+    // if (a%5==0 || a%3==0){
+
+    //     cout<<"true"<<endl;
+    // }
+    // else{
+    //     cout<<"false"<<endl;
+    // }
+
+    // int b;
+    // cout<<"enter b no :";
+    // cin>>b;
+    // if (b%5==0  && b%3==0){
+    //     cout<<"true"<<endl;
+    // }
+    // else{
+    //     cout<<"false"<<endl;
+    // }
+
+    // int a;
+    // cout<<"enter a no :";
+    // cin>>a;
+    // if (a>99 || a<1000){
+
+    //     cout<<"three digit no"<<endl;
+    // }
+    // else{
+    //     cout<<"not three digit no"<<endl;
+    // // }
+    // int a;
+    // int b;
+    // int c;
+    // cout<<"enter a no:";
+    // cin>>a;
+    // cout<<"enter b no:";
+    // cin>>b;
+    // cout<<"enter c no:";
+    // cin>>c;
+
+    // if(a>b && a>c){
+    //     cout<<"the number is a grether than all"<<" "<<a<<endl;
+    // }
+
+    // else if(b>a && b>c){
+    //      cout<<"the number is b grether than all"<<" "<<b<<endl;
+    // }
+    // else if(c>a && c>b){
+    //     cout<<"the number is c grether than all"<<" "<<c<<endl;
+    // }
+    // else{
+    //     cout<<"all equal";
+    // }
+
+    // char h;
+    // cin>>h;
+    // if(h>=97 && h<=122 || h>=65 && h<=90){
+    //     cout<<"this is correct";
+    // }
+    // else{
+    //     cout<<"this is incorrect";
+    // }
+    // int a;
+    // cin>>a;
+    // if(a%5==0){
+    //     if (a%3==0){
+    //         cout<<" no is correct";
+    //     }
+    //     else
+    //     {
+    //       cout<<"no is not correct";
+    //     }
+    // }
+    // else{
+    //     cout<<"no";
+    // }
+    // int a;
+    // int b;
+    // int c;
+    // cin >> a;
+    // cin >> b;
+    // cin >> c;
+    // if (a > b)
+    // {
+    //     if (a > c)
+    //     {
+    //         cout << "a is greater than all";
+    //     }
+    // }
+    // else if (b > a)
+    // {
+    //     if (b > c)
+    //     {
+    //         cout << "b is greater than all";
+    //     }
+    // }
+    // else if (c > a)
+    // {
+    //     if (c > b)
+    //     {
+    //         cout << "c is greater than all";
+    //     }
+    // }
+    // else
+    // {
+    //     cout << "koi greater than nhi h";
+    // }
+
+// char x;
+// cout<<"enter youe char:";
 // cin>>x;
-// cout<<x;
+// if(x=='a' || x=='e' || x=='i' || x=='o' || x=='u'){
+//     cout<<"this is vowel";
+// }
+// else{
+//     cout<<"this is not vowel";
+// }
+// int a;
+// int b;
+// int c;
+// cin>>a;
+// cin>>b;
+// cin>>c;
+// if(a+b>c || b+c>a || c+a>b){
+//     cout<<"this is a triangle";
+// }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
+// else{
+//     cout<<"this is a not triangle";
+// }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+// int a=3;
+// (a==5)?cout<<"true":cout<<"false";
 // int x;
 // int y;
+// char op;
 // cin>>x;
+// cin>>op;
 // cin>>y;
-// int a=x+y;
-// a=x-y;
-// cout<<a;
-// int r;
-// cin>>r;
-// int pi=22/7;
-// int c=pi*r*r;
-// // cout<<c;
-// int a=5;
-// int b=7;
-// cout<<(a==b)<<endl;
-// cout<<(a<b)<<endl;
-// cout<<(a>b)<<endl;
-// cout<<(a!=b)<<endl;
-// cout<<(a<=b)<<endl;
-// cout<<(a>=b)<<endl;
-// int a=55;
-// int b=66;
-// cout<<a+b;
-// int boy=16;
-// if(boy>18){
-//     cout<<"vote de payega";
-// }
-// else{
-//     cout<<"vote nhi de payega";
-// }
-// int b;
-// cin>>b;
-// if(b%2==0){
-//     cout<<"true";
-//     }
-// else{
-// cout<<"false";
-// }
-// int a;
-// cin>>a;
-// if(a>90){
-//     cout<<"very good";
+// switch(op){
+//     case '+':
+//     cout<<x+y;
+//     break;
+//     case '-':
+//     cout<<x-y;
+//     break;
+//     case '*':
+//     cout<<x*y;
+//     break;
+//     case '/':
+//     cout<<x/y;
+//     break;
     
-// }
-// else if(a>80){
-//     cout<<"good";
-// }
-// else if(a>60){
-//     cout<<"normal";
-// }
-// else{
-//     cout<<"fail";
-// // }
-// int a;
-
-// cin>>a;
-// cout<<"enter my no: "; 
-// if(a>0){
-//     cout<<"positive";
-// }
-// else{
-//     cout<<"nagetive";
-// }
-// int a;
-// int b;
-// cin>>a;
-// cin>>b;
-// if (a<b){
-//     cout<<"profit"<<endl;
-//     cout<<(b-a);
-// }
-// else{
-//     cout<<"lose"<<endl;
-//     cout<<(a-b);
-// }
-// int a;
-// cout<<"enter a no :";
-// cin>>a;
-// if (a%5==0 || a%3==0){
-
-//     cout<<"true"<<endl;
-// }
-// else{
-//     cout<<"false"<<endl;
-// }
-
-// int b;
-// cout<<"enter b no :";
-// cin>>b;
-// if (b%5==0  && b%3==0){
-//     cout<<"true"<<endl;
-// }
-// else{
-//     cout<<"false"<<endl;
+    
+//     default:
+//     cout<<"invalid";
 // }
 
 
-// int a;
-// cout<<"enter a no :";
-// cin>>a;
-// if (a>99 || a<1000){
 
-//     cout<<"three digit no"<<endl;
-// }
-// else{
-//     cout<<"not three digit no"<<endl;
-// }
-int a;
-int b;
-int c;
-cout<<"enter a no:";
-cin>>a;
-cout<<"enter b no:";
-cin>>b;
-cout<<"enter c no:";
-cin>>c;
 
-if(a>b && a>c){
-    cout<<"the number is a grether than all"<<" "<<a<<endl;
-}
 
-else if(b>a && b>c){
-     cout<<"the number is b grether than all"<<" "<<b<<endl;
-}
-else if(c>a && c>b){
-    cout<<"the number is c grether than all"<<" "<<c<<endl;
-}
-else{
-    cout<<"all equal";
-}
 
 
 
